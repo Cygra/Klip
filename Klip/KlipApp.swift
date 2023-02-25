a2f1ccfc99c45a9345db31dffe4a8b9bda6a88bd
Cygra <sjtuwbh@gmail.com>
2023-02-25 11:47:26 +0800

    Initial Commit

---
+++ Klip/KlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  KlipApp.swift
+//  Klip
+//
+//  Created by 王博晗 on 2023/2/25.
+//
+
+import SwiftUI
+
+@main
+struct KlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
